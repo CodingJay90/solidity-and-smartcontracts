64feb36550aa4d3f642339ec09a2a1bf68b4fc67
Faruq <CodingJay90>
2022-03-02 04:21:37 +0100

    updates

--- inbox-updated/deploy.js
+++ inbox-updated/deploy.js
@@ -1,20 +1,25 @@
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 const Web3 = require("web3");
-const { interface, bytecode } = require("./compile");
 
-const provider = new HDWalletProvider(
+const { abi, evm } = require("./compile");
+
+provider = new HDWalletProvider(
   "remember swift soldier code blanket verify top item genre fade fitness question",
   "https://rinkeby.infura.io/v3/d8e420b4670e407aa56d9ca8473e09ab"
 );
+
 const web3 = new Web3(provider);
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
-  console.log("deploying", accounts);
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ["Hi there!"] })
-    .send({ from: accounts[0], gas: "1000000" });
-  console.log("result", result.options.address);
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ["Hi there!"] })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
   provider.engine.stop();
 };
 
